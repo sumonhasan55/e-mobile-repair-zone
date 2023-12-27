e710076257cc629876b7af93e6298a9213a2790f
sumonhasan55 <wubsumon7577@gmail.comm>
2023-12-27 20:22:38 +0600

    first time install allpackage

---
+++ src/app/home/page.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const HomePage = () => {
+    return (
+        <div>
+            <h2>Best services</h2>
+            
+        </div>
+    );
+};
+
+export default HomePage;
---
+++ src/app/login/page.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const login = () => {
+    return (
+        <div>
+            <h1>Log in Now</h1>
+        </div>
+    );
+};
+
+export default login;
